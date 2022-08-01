de4f29099c0513884e5db326fdecba2d22d92af8
Arushigandhi <arushi.gandhi@gmail.com>
2022-08-01 15:24:16 +0530

    chnaged url

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,6 +1,6 @@
 // import store from "store";
 
-export const BACKEND_URL = "http://localhost:8080/api";
+export const BACKEND_URL = "https://flipdapp-grid.herokuapp.com/api";
 
 // export const GetJWTToken = () => {
 //   console.log(store.getState());
